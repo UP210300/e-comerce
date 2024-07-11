6e2040c3409d15ac05cad5b0d116d49c5e680d8f
Abel <up210894@alumnos.upa.edu.mx>
2024-07-10 23:28:19 -0600

    Database Changes

---
+++ backend/src/main/resources/CreateUser.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS FULL;
+
+-- drop user 'full'@'localhost';     sensible a Mayusculas
+-- SET PASSWORD FOR  'full'@'localhost' = PASSWORD ('full')
+
+CREATE USER 'full'@'localhost' IDENTIFIED BY 'full';
+
+GRANT ALL PRIVILEGES ON FULL.* TO 'full'@'localhost' [identified by 'full'];
+
+FLUSH PRIVILEGES;
