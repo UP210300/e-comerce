1a9069cef1e65a7cdf1a51bcbe782cd424136e9b
Sofia Calderon <up210300@alumnos.upa.edu.mx>
2024-07-11 18:34:41 -0600

    Added main layout

---
+++ frontend/src/components/Cart/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Cart() {
+  return (
+    <div className="pt-96">
+      aaa
+    </div>
+  );
+}
+
+export default Cart;
---
+++ frontend/src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Carousel from '../Carousel/Carousel';
+import Categories from '../Categories/Category';
+
+function Home() {
+  return (
+    <div>
+      <Carousel />
+      <Categories />
+    </div>
+  );
+}
+
+export default Home;
