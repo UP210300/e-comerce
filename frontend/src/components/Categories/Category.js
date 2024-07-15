256b9015c6a2d60dcb2334296a8bd599908815d3
UP210908 <112641150+UP210908@users.noreply.github.com>
2024-07-15 17:30:58 -0600

    Changes in the category section

--- frontend/src/components/Categories/Category.js
+++ frontend/src/components/Categories/Category.js
@@ -1,19 +1,51 @@
-
-
 import React from 'react'; 
 import { Card } from 'primereact/card';
 
 export default function BasicDemo() {
+    const headerStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '0' 
+    };
+
     const header = (
-        <img alt="Card" src="assets/default image" />
+        <div style={headerStyle}>
+            <img src={require('../assets/default-image.jpg')} alt="" />
+        </div>
     );
 
+    const cardTitleStyle = {
+        textAlign: 'center',
+        margin: '0'
+
+    };
+
+    const cardStyle = {
+        width: '30%', // Ajusta este valor para cambiar el ancho
+        margin: '10px' // Añade un margen para espaciar las tarjetas
+    };
+
     return (
-        <div className="card">
-            <Card title="Simple Card" header={header}>
-                <h2>Encabezado del Card</h2>
-            </Card>
+        <div>
+            <hr style={{ border: '0', height: '2px', background: '#ccc', margin: '20px 0' }} />
+            <div className="card-container" style={{ display: 'flex', justifyContent: 'space-around', marginTop: '20px' }}>
+                <Card header={header} style={cardStyle}>
+                    <div style={cardTitleStyle}>
+                        <h1 style={{ margin: '0' }}>Category 1</h1> 
+                    </div>
+                </Card>
+                <Card header={header} style={cardStyle}>
+                    <div style={cardTitleStyle}>
+                        <h1 style={{ margin: '0' }}>Category 2</h1> 
+                    </div>
+                </Card>
+                <Card header={header} style={cardStyle}>
+                    <div style={cardTitleStyle}>
+                        <h1 style={{ margin: '0' }}>Category 3</h1> 
+                    </div>
+                </Card>
+            </div>
         </div>
     );
 }
-
