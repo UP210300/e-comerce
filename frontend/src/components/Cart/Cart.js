2992a89f1cd1352d68ec091e2f7ace7b95b71785
Sofia Calderon <up210300@alumnos.upa.edu.mx>
2024-07-15 15:20:56 -0600

    Added cart layout

--- frontend/src/components/Cart/Cart.js
+++ frontend/src/components/Cart/Cart.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useLocation, Outlet } from 'react-router-dom';
+import CartProduct from '../Product/CartProduct';
+import OrderSumary from './OrderSumary';
+import PaymentMethod from './PaymentMethods';
 
 function Cart() {
+  const location = useLocation();
+  const isCheckoutPage = location.pathname === '/pagar';
+
   return (
-    <div>
-      Carrito
+    <div className="grid md:grid-cols-2">
+      <CartProduct/>
+      <div>
+        <OrderSumary/>
+        {isCheckoutPage &&<PaymentMethod/>}
+      </div>
     </div>
   );
 }
--- frontend/src/components/Cart/OrderSumary.js
+++ frontend/src/components/Cart/OrderSumary.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Button } from 'primereact/button';
+import { Link } from 'react-router-dom';
 
 export default function BasicDemo() {
     return (
-        <div className="flex justify-center">
-            <div className="ml-4 w-1/4">
-                <div className="border-b border-black pb-2">
-                    <p className="font-bold text-xl mb-1">Resumen del pedido</p>
-                    <p>Total de artículos:</p>
-                    <p>Total:</p>
-                </div>
-                <Button label="Comprar y pagar" className="bg-slate-300 w-full mt-4" />
+        <div className="p-10">
+            <div className="border-b border-black">
+                <p className="font-bold text-xl">Resumen del pedido</p>
+                <p>Total de artículos:</p>
+                <p>Total:</p>    
             </div>
+            <Link to="/pagar">
+                <Button label="Comprar y pagar" className="bg-slate-300 w-full mt-4" />
+            </Link>
         </div>
     );
 }
