5b7056044250f17ce71d0e0c2569b9785cce6265
Abel <up210894@alumnos.upa.edu.mx>
2024-07-15 19:40:11 -0600

    add route home

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Carousel from '../Carousel/Carousel';
 import Categories from '../Categories/Category';
+import Product from '../Product/Product';
 
 function Home() {
   return (
     <div>
       <Carousel />
       <Categories />
+      <Product />
     </div>
   );
 }
