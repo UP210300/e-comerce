d12e327270967386e933d56f4e0d001ca0d06504
Sofia Calderon <up210300@alumnos.upa.edu.mx>
2024-07-18 15:22:09 -0600

    Added login and register

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -6,10 +6,12 @@ import CheckoutBar from '../NavBar/CheckoutBar';
 function App() {
   const location = useLocation();
   const isCheckoutPage = location.pathname === '/pagar';
+  const isLoginPage = location.pathname === '/iniciar-sesion';
+  const isRegisterPage = location.pathname === '/registrarme';
 
   return (
     <div>
-      {!isCheckoutPage &&<NavBar />}
+      {!isCheckoutPage && !isLoginPage && !isRegisterPage && <NavBar />}
       {isCheckoutPage && <CheckoutBar />}
       <div className="px-10 g:px-20 py-10">
         <Outlet /> 
