ba78185a090cdd4550fd2e2bc552790936499768
Paulina Jaquelin Alvarez Martinez <up210374@alumnos.upa.edu.mx>
2024-07-19 17:45:16 -0600

    Added mappers and dto structure

---
+++ backend/src/main/java/com/ecomerce/e_comerce/exception/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package com.ecomerce.e_comerce.exception;
+
+public class ProductNotFoundException extends java.lang.RuntimeException {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
+
