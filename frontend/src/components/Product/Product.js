7e55d09e112e2669034b580ab81dc2fc0f0399b8
Jeannelyn Avila <up210163@alumnos.upa.edu.mx>
2024-07-22 16:59:49 -0600

    Create notification for product added to cart

--- frontend/src/components/Product/Product.js
+++ frontend/src/components/Product/Product.js
@@ -1,24 +1,45 @@
-import React from 'react'; 
+import React, { useRef } from 'react';
 import { Card } from 'primereact/card';
+import { Toast } from 'primereact/toast';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { useCart } from '../context/CartContext';
 
 export default function AdvancedDemo() {
+    const { addToCart } = useCart();
+    const toast = useRef(null);
+
     const header = (
-        <img src={require('../assets/default-image.jpg')} alt="" className="w-full h-full object-cover"></img>
+        <img src={require('../assets/default-image.jpg')} alt="" className="w-full h-full object-cover" />
     );
-    const footer = (
-        <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
-            <div style={{ flexGrow: 1 }}>
-                <h3 style={{ margin: 0 }}>Nombre del Producto</h3>
+
+    const footer = (product) => (
+        <div className="flex justify-end items-center">
+            <div className="flex-grow">
+                <h3 className="m-0">{product.name}</h3>
             </div>
-            <FontAwesomeIcon icon={faShoppingCart} className="text-gray-500 hover:text-blue-500 cursor-pointer" />
+            <FontAwesomeIcon
+                icon={faShoppingCart}
+                className="text-gray-500 hover:text-blue-500 cursor-pointer"
+                onClick={() => handleAddToCart(product)}
+            />
         </div>
     );
 
-    const cards = Array.from({ length: 8 }).map((_, index) => (
-        <Card key={index} footer={footer} header={header} className="md:w-25rem" style={{ margin: '0.5em', flex: '1' }}>
-            {/* Contenido opcional dentro de la tarjeta */}
+    const products = Array.from({ length: 8 }).map((_, index) => ({
+        id: index,
+        name: `Producto ${index + 1}`,
+        price: 100,
+        image: require('../assets/default-image.jpg')
+    }));
+
+    const handleAddToCart = (product) => {
+        addToCart(product);
+        toast.current.show({ severity: 'success', summary: 'Producto Añadido', detail: `El producto ${product.name} ha sido añadido al carrito`, life: 3000 });
+    };
+
+    const cards = products.map((product) => (
+        <Card key={product.id} footer={footer(product)} header={<img src={product.image} alt="" className="w-full h-full object-cover" />} className="md:w-1/4 m-2 flex-1">
         </Card>
     ));
 
@@ -26,11 +47,12 @@ export default function AdvancedDemo() {
     const secondRow = cards.slice(4, 8);
 
     return (
-        <div style={{ margin: '2em 0' }}>
-            <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', flexWrap: 'nowrap' }}>
+        <div className="my-8">
+            <Toast ref={toast} />
+            <div className="flex justify-around items-center flex-nowrap">
                 {firstRow}
             </div>
-            <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', flexWrap: 'nowrap', marginTop: '1em' }}>
+            <div className="flex justify-around items-center flex-nowrap mt-4">
                 {secondRow}
             </div>
         </div>
