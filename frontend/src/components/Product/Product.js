518366d53fe6dce262806848bc9412512a380db0
UP210163 <up210163@alumnos.upa.edu.mx>
2024-07-29 22:12:47 -0600

    Resolve issues in product and cart

--- frontend/src/components/Product/Product.js
+++ frontend/src/components/Product/Product.js
@@ -4,6 +4,7 @@ import { Toast } from 'primereact/toast';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { useCart } from '../context/CartContext';
+import { Link } from 'react-router-dom';
 
 export default function AdvancedDemo() {
     const { addToCart } = useCart();
@@ -11,13 +12,15 @@ export default function AdvancedDemo() {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:8585/api/products')
+        fetch('http://localhost:8080/api/products')
             .then(response => response.json())
             .then(data => setProducts(data))
             .catch(error => console.error('Error fetching products:', error));
     }, []);
 
-    const handleAddToCart = (product) => {
+    const handleAddToCart = (event, product) => {
+        event.stopPropagation();
+        event.preventDefault();
         addToCart(product);
         toast.current.show({ severity: 'success', summary: 'Producto Añadido', detail: `El producto ${product.name} ha sido añadido al carrito`, life: 3000 });
     };
@@ -30,21 +33,22 @@ export default function AdvancedDemo() {
             <FontAwesomeIcon
                 icon={faShoppingCart}
                 className="text-gray-500 hover:text-blue-500 cursor-pointer"
-                onClick={() => handleAddToCart(product)}
+                onClick={(event) => handleAddToCart(event, product)}
             />
         </div>
     );
 
     const cards = products.map((product) => (
-        <Card key={product.id} footer={footer(product)} header={<img src={require('../assets/default-image.jpg')} alt="" className="w-full h-full object-cover" />} className="md:w-1/4 m-2 flex-1">
-            <p>{product.description}</p>
-            <p>Precio: ${product.price}</p>
-            <p>Stock: {product.stock}</p>
-        </Card>
+        <Link to={`/detalle-de-producto/${product.id}`} className="md:w-1/3 m-6 flex-6 no-underline" style={{ textDecoration: 'none' }} key={product.id}>
+            <Card footer={footer(product)} header={<img src={require('../assets/default-image.jpg')} alt="" className="w-full h-full object-cover"/>} className="w-full h-full object-cover">
+                <p>{product.description}</p>
+                <p>Precio: ${product.price}</p>
+            </Card>
+        </Link>
     ));
 
-    const firstRow = cards.slice(0, 4);
-    const secondRow = cards.slice(4, 8);
+    const firstRow = cards.slice(0, 3);
+    const secondRow = cards.slice(3, 6);
 
     return (
         <div className="my-8">
