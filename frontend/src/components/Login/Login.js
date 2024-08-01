d71d9e453c9a7324f25006e9f7d8d68bde5f7a01
Sofia Calderon <up210300@alumnos.upa.edu.mx>
2024-07-31 23:39:56 -0600

    Update register view

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { InputText } from "primereact/inputtext";
+import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const Login = () => {
   const [usernameOrEmail, setUsernameOrEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [showPassword, setShowPassword] = useState(false); // Nuevo estado para mostrar/ocultar contraseña
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -41,15 +42,15 @@ const Login = () => {
       <div className="absolute left-10 top-10">
         <Link to="/">
           <button className="flex flex-row space-x-2 text-primary-500">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
-              <path fill-rule="evenodd" d="M18 10a.75.75 0 0 1-.75.75H4.66l2.1 1.95a.75.75 0 1 1-1.02 1.1l-3.5-3.25a.75.75 0 0 1 0-1.1l3.5-3.25a.75.75 0 1 1 1.02 1.1l-2.1 1.95h12.59A.75.75 0 0 1 18 10Z" clip-rule="evenodd" />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+              <path fillRule="evenodd" d="M18 10a.75.75 0 0 1-.75.75H4.66l2.1 1.95a.75.75 0 1 1-1.02 1.1l-3.5-3.25a.75.75 0 0 1 0-1.1l3.5-3.25a.75.75 0 1 1 1.02 1.1l-2.1 1.95h12.59A.75.75 0 0 1 18 10Z" clipRule="evenodd" />
             </svg>
             <p>Regresar al inicio</p>
           </button>
         </Link>
       </div>
       <div className="flex flex-col items-center justify-center h-screen space-y-10">
-        <div className="p-6 w-full max-w-md space-y-5">
+        <div className="border border-gray-200 shadow-md rounded-lg p-8 w-full max-w-md space-y-5">
           <img src='/assets/logo.png' alt="Logo" />
           <h2 className="text-center font-semibold text-2xl text-primary-500 mb-6">Iniciar sesión</h2>
           <form onSubmit={handleSubmit} className="space-y-5">
@@ -62,14 +63,21 @@ const Login = () => {
                 required
               />
             </div>
-            <div className="flex flex-col">
+            <div className="flex flex-col relative">
               <label>Contraseña</label>
               <InputText
-                type="password"
+                type={showPassword ? 'text' : 'password'}
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
               />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute inset-y-6 right-0 flex items-center h-2/3 pr-3"
+              >
+                <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} className="text-gray-500" />
+              </button>
             </div>
             {error && <p style={{ color: 'red' }}>{error}</p>}
             <Button label="Iniciar sesión" type="submit" className="bg-primary-500 p-2 w-full" />
@@ -82,7 +90,7 @@ const Login = () => {
           </div>
         </div>
       </div>
-    </div> 
+    </div>
   );
 };
 
