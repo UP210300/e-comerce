9a13d3f2e5ca84328b7bdc196a6a120d325dee72
Sofia Calderon <up210300@alumnos.upa.edu.mx>
2024-07-31 18:42:42 -0600

    Added user dto and mapper

---
+++ backend/src/main/java/com/ecomerce/e_comerce/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ecomerce.e_comerce.exception;
+
+public class UserNotFoundException extends java.lang.RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
